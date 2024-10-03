6a0c54130f7acde5271fb401fd86e8f106b0ad30
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-10-02 23:53:07 -0300

    Create 4-busca_google.py

---
+++ 4-busca_google.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# 1 - Termo de pesquisa
+term = input('Digite o que deseja pesquisar:\n')
+
+# 2 - Iniciando o Driver
+browser = webdriver.Firefox()
+browser.get('https://www.google.com.br/')
+
+# 3 - Encontrando o elemento
+elem = browser.find_element(
+    By.XPATH,
+    "//textarea[@aria-label]"
+    )
+
+# 4 - Enviando termo para pesquisa
+elem.send_keys(term)
+elem.send_keys(Keys.ENTER)
