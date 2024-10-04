dbb8deeb1e6d024ebe03c5aee3a6490ec79ad316
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-10-04 16:48:08 -0300

    Update 4-busca_google.py

--- 4-busca_google.py
+++ 4-busca_google.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 # 1 - Termo de pesquisa
 term = input('Digite o que deseja pesquisar:\n')
 
@@ -15,7 +14,7 @@ browser.get('https://www.google.com.br/')
 elem = browser.find_element(
     By.XPATH,
     "//textarea[@aria-label]"
-    )
+)
 
 # 4 - Enviando termo para pesquisa
 elem.send_keys(term)
@@ -24,6 +23,15 @@ elem.send_keys(Keys.ENTER)
 # 5 - Retornando a quantidade de registros
 time.sleep(2)
 results = browser.find_element(By.ID, 'result-stats').text
-print(f'foram encontrados {results}')
+print(f'Foram encontrados {results}')
+
+# 6 - Retornando número de páginas
+# Corrigindo a extração do número de resultados
+try:
+    qtd_results = int(results.split('Aproximadamente ')[1].split(' resultados')[0].replace('.', '').replace(',', ''))
+    tot_pages = qtd_results / 10  # Você pode ajustar o valor dependendo da configuração
+    print(f'Número de páginas: {int(tot_pages)}')
+except Exception as e:
+    print("Não foi possível calcular o número de páginas.", e)
 
 browser.quit()
