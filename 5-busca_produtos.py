95e7d920c3c735895dc376c4de293d03b2b85d46
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-04 22:18:35 -0300

    Update 5-busca_produtos.py

--- 5-busca_produtos.py
+++ 5-busca_produtos.py
@@ -21,3 +21,43 @@ element = browser.find_element(
 time.sleep(2)
 print(element)
 time.sleep(2)
+
+# 4 - encontrar informações do produto
+items = element.find_elements(
+    By.XPATH,
+    '//div[@data-component-type="s-search-result"]'
+)
+
+print(len(items))
+
+for item in items:
+    title = item.find_element(By.TAG_NAME, 'h2').text
+    price = ""
+    link = item.find_element(
+        By.CLASS_NAME,
+        'a-link-normal'
+        ).get_attribute('href')
+    img = ''
+    
+    try:
+        price = item.find_element(
+            By.CLASS_NAME,
+            'a-price'
+        ).text.replace('\n ', '.')
+    except:
+        pass
+    
+    try:
+        img = item.find_element(
+            By.CLASS_NAME,
+            's-image'
+        ).get_attribute('src')
+    except:
+        pass
+    
+    print(f"tÍTULO: {title}")
+    print(f"Preço: {price}")
+    print(f"Link: {link}")
+    print(f"Imagem: {img}")
+
+browser.quit()
